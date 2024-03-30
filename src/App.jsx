1dc2aca6a0f2fe605d4e6315f2d5ba2fb9c8e3fe
Eduardo Gonzalez <heyimedd@gmail.com>
2024-03-30 03:25:34 -0600

    first commit2

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import data from './data';
+import { List } from './List';
+const App = () => {
+  const [people, setPeople] = useState(data);
+  console.log(people);
+  return (
+    <main>
+      <section className="container">
+        <h3>{people.length} Birthdays Today</h3>
+        <List people={people} />
+        {people.length > 0 ? (
+          <button
+            className="btn-block btn"
+            onClick={() => {
+              setPeople([]);
+            }}
+          >
+            Clear list
+          </button>
+        ) : (
+          <button
+            className="btn btn-block"
+            onClick={() => {
+              setPeople(data);
+            }}
+          >
+            Refresh
+          </button>
+        )}
+      </section>
+    </main>
+  );
+};
+export default App;
---
+++ src/List.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Person from './Person';
+
+export const List = ({ people }) => {
+  return (
+    <section>
+      {people.map((person) => {
+        return <Person key={person.id} {...person} />;
+      })}
+    </section>
+  );
+};
